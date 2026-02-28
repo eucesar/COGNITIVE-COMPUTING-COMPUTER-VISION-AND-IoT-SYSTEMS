a56924ddf31eb0fd114fd673fe9348030e620ae3
Cesar Iglesias <cesaribneto.job@gmail.com>
2026-02-27 22:40:01 -0300

    esse

---
+++ 1-aula/primeira-aula/main.py
@@ -0,0 +1,12 @@
+print("ola mundo")
+
+soma = input("digite um numero")
+
+def foo():
+    a = input("digite uma coisa")
+    b = input("digite uma coisa")
+    return a, b
+
+valor1, valor2 = foo()
+
+print(valor1, valor2)
